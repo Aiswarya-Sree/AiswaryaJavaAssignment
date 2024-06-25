7adfb559d2a3c448a36903536bd90a5c593a5a7b
aiswarya.sree1 <aiswarya.sree1@SPCLTLAP-0422.speridian.com>
2024-06-25 19:22:41 +0530

    assignment

---
+++ Assignment/src/main/java/Assignment1/sum.java
@@ -0,0 +1,12 @@
+package Assignment1;
+
+public class sum {
+static int a=5;
+static int b=3;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int c=a+b;
+System.out.println("sum is "+c);
+	}
+
+}
